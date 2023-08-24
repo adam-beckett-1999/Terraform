54cd9c017319c17aea935700e71cf14ddd107537
Adam Beckett <becketta@live.com>
2023-08-24 13:51:51 +0100

    Create vm-swarm-02.tf

---
+++ proxmox/vm-swarm-02.tf
@@ -0,0 +1,34 @@
+resource "proxmox_vm_qemu" "vm_swarm_02" {
+    target_node = "phs-hv-01"
+    vmid = "202"
+    name = "VM-SWARM-02"
+    tags = "app-server,docker,glusterfs"
+
+    clone = "UBUNTU-22.04"
+    full_clone = true
+
+    onboot = true
+    agent = 1
+    cores = 4
+    sockets = 2
+    cpu = "host"
+    memory = 8192
+
+    disk {
+        type = "scsi"
+        storage = "pool-1"
+        size = "200G"
+    }
+
+    network {
+        bridge = "vmbr0"
+        model = "virtio"
+    }
+
+    os_type = "cloud-init"
+    ipconfig0 = "ip=192.168.0.162/24,gw=192.168.0.1"
+    ciuser = var.vm_username
+    cipassword = var.vm_password
+    sshkeys = var.vm_ssh_key
+    nameserver = "192.168.0.210"
+}
