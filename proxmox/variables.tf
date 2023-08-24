66e1aaed84ee1ef7dd95159f71637a1c12091aae
Adam Beckett <becketta@live.com>
2023-08-24 13:49:56 +0100

    Create variables.tf

---
+++ proxmox/variables.tf
@@ -0,0 +1,18 @@
+variable "proxmox_api_url" {
+	type = string
+}
+variable "proxmox_username" {
+	type = string
+}
+variable "proxmox_password" {
+	type = string
+}
+variable "vm_username" {
+	type = string
+}
+variable "vm_password" {
+	type = string
+}
+variable "vm_ssh_key" {
+	type = string
+}
