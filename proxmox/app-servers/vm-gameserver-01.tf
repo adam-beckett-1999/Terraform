e4f93e85854e74b955815f72093bd36c806f0d73
Adam Beckett <becketta@live.com>
2023-08-26 17:11:10 +0100

    updated

--- proxmox/app-servers/vm-gameserver-01.tf
+++ proxmox/app-servers/vm-gameserver-01.tf
@@ -1,6 +1,6 @@
 resource "proxmox_vm_qemu" "vm_gameserver_01" {
     target_node = "phs-hv-01"
-    vmid = "206"
+    vmid = "221"
     name = "VM-GAMESERVER-01"
     tags = "app-server,docker,pufferpanel"
 
@@ -26,7 +26,7 @@ resource "proxmox_vm_qemu" "vm_gameserver_01" {
     }
 
     os_type = "cloud-init"
-    ipconfig0 = "ip=192.168.0.166/24,gw=192.168.0.1"
+    ipconfig0 = "ip=192.168.0.181/24,gw=192.168.0.1"
     ciuser = var.vm_username
     cipassword = var.vm_password
     sshkeys = var.vm_ssh_key
