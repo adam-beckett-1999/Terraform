c535494992a480d4d1f746ec25d609ac3011a9f9
Adam Beckett <becketta@live.com>
2024-02-06 22:05:47 +0000

    updated

--- proxmox/app-servers/variables.tf
+++ proxmox/app-servers/variables.tf
@@ -7,12 +7,3 @@ variable "proxmox_username" {
 variable "proxmox_password" {
   type = string
 }
-variable "vm_username" {
-  type = string
-}
-variable "vm_password" {
-  type = string
-}
-variable "vm_ssh_key" {
-  type = string
-}
