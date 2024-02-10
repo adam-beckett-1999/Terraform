4902ce512b16c9ae32c6249e93e4c5bc964c8bdc
Adam Beckett <becketta@live.com>
2024-02-10 15:25:59 +0000

    updated

--- proxmox/docker-servers/resources.tf
+++ proxmox/docker-servers/resources.tf
@@ -1,47 +1,5 @@
-resource "proxmox_virtual_environment_vm" "vm_docker_proxy" {
-    name = "VM-DOCKER-PROXY"
-    node_name = "HYPERVISOR-01"
-    vm_id = 100
-    tags = ["docker", "glusterfs", "ubuntu-2204"]
-    on_boot = true
-    started = false
-    keyboard_layout = "en-gb"
-    agent {
-        enabled = true
-    }
-    clone {
-        vm_id = 9000
-        full = true
-    }
-    cpu {
-        cores = 4
-        sockets = 2
-    }
-    disk {
-        datastore_id = "storage-01-iscsi"
-        interface = "scsi0"
-        size = 100
-    }
-    memory {
-        dedicated = 8192
-    }
-    network_device {
-        bridge = "vmbr0"
-        model = "virtio"
-    }
-    initialization {
-        datastore_id = "local-lvm"
-        interface = "ide0"
-        ip_config {
-            ipv4 {
-                address = "192.168.0.160/24"
-                gateway = "192.168.0.1"
-            }
-        }
-    }
-}
-resource "proxmox_virtual_environment_vm" "vm_docker_media" {
-    name = "VM-DOCKER-MEDIA"
+resource "proxmox_virtual_environment_vm" "vm_docker_server-01" {
+    name = "VM-DOCKER-SERVER-01"
     node_name = "HYPERVISOR-01"
     vm_id = 101
     tags = ["docker", "glusterfs", "ubuntu-2204"]
@@ -82,8 +40,8 @@ resource "proxmox_virtual_environment_vm" "vm_docker_media" {
         }
     }
 }
-resource "proxmox_virtual_environment_vm" "vm_docker_services" {
-    name = "VM-DOCKER-SERVICES"
+resource "proxmox_virtual_environment_vm" "vm_docker_server_02" {
+    name = "VM-DOCKER-SERVER-02"
     node_name = "HYPERVISOR-01"
     vm_id = 102
     tags = ["docker", "glusterfs", "ubuntu-2204"]
@@ -124,11 +82,11 @@ resource "proxmox_virtual_environment_vm" "vm_docker_services" {
         }
     }
 }
-resource "proxmox_virtual_environment_vm" "vm_docker_management" {
-    name = "VM-DOCKER-MANAGEMENT"
+resource "proxmox_virtual_environment_vm" "vm_docker_server_03" {
+    name = "VM-DOCKER-SERVER-03"
     node_name = "HYPERVISOR-01"
     vm_id = 103
-    tags = ["docker", "glusterfs", "portainer", "ubuntu-2204"]
+    tags = ["docker", "glusterfs", "ubuntu-2204"]
     on_boot = true
     started = false
     keyboard_layout = "en-gb"
@@ -161,6 +119,48 @@ resource "proxmox_virtual_environment_vm" "vm_docker_management" {
         ip_config {
             ipv4 {
                 address = "192.168.0.163/24"
+                gateway = "192.168.0.1"
+            }
+        }
+    }
+}
+resource "proxmox_virtual_environment_vm" "vm_docker_server-04" {
+    name = "VM-DOCKER-SERVER-04"
+    node_name = "HYPERVISOR-01"
+    vm_id = 104
+    tags = ["docker", "glusterfs", "portainer", "ubuntu-2204"]
+    on_boot = true
+    started = false
+    keyboard_layout = "en-gb"
+    agent {
+        enabled = true
+    }
+    clone {
+        vm_id = 9000
+        full = true
+    }
+    cpu {
+        cores = 4
+        sockets = 2
+    }
+    disk {
+        datastore_id = "storage-01-iscsi"
+        interface = "scsi0"
+        size = 100
+    }
+    memory {
+        dedicated = 8192
+    }
+    network_device {
+        bridge = "vmbr0"
+        model = "virtio"
+    }
+    initialization {
+        datastore_id = "local-lvm"
+        interface = "ide0"
+        ip_config {
+            ipv4 {
+                address = "192.168.0.164/24"
                 gateway = "192.168.0.1"
             }
         }
