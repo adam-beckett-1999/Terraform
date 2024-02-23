21a28c31a2963895db42143544eba04d12afd817
Adam Beckett <becketta@live.com>
2024-02-23 11:16:32 +0000

    updated

--- proxmox/kubernetes-cluster/resources.tf
+++ proxmox/kubernetes-cluster/resources.tf
@@ -1,6 +1,6 @@
 resource "proxmox_virtual_environment_vm" "vm_k3s_server_01" {
     name = "VM-K3S-SERVER-01"
-    node_name = "HYPERVISOR-01"
+    node_name = "HYPERVISOR-02"
     vm_id = 201
     tags = ["docker", "glusterfs", "ubuntu-2204"]
     on_boot = true
@@ -42,7 +42,7 @@ resource "proxmox_virtual_environment_vm" "vm_k3s_server_01" {
 }
 resource "proxmox_virtual_environment_vm" "vm_k3s_server_02" {
     name = "VM-K3S-SERVER-02"
-    node_name = "HYPERVISOR-01"
+    node_name = "HYPERVISOR-02"
     vm_id = 202
     tags = ["docker", "glusterfs", "ubuntu-2204"]
     on_boot = true
@@ -84,7 +84,7 @@ resource "proxmox_virtual_environment_vm" "vm_k3s_server_02" {
 }
 resource "proxmox_virtual_environment_vm" "vm_k3s_server_03" {
     name = "VM-K3S-SERVER-03"
-    node_name = "HYPERVISOR-01"
+    node_name = "HYPERVISOR-02"
     vm_id = 203
     tags = ["docker", "glusterfs", "ubuntu-2204"]
     on_boot = true
@@ -126,7 +126,7 @@ resource "proxmox_virtual_environment_vm" "vm_k3s_server_03" {
 }
 resource "proxmox_virtual_environment_vm" "vm_k3s_server_04" {
     name = "VM-DOCKER-SERVER-04"
-    node_name = "HYPERVISOR-01"
+    node_name = "HYPERVISOR-02"
     vm_id = 204
     tags = ["docker", "glusterfs", "portainer", "ubuntu-2204"]
     on_boot = true
@@ -168,7 +168,7 @@ resource "proxmox_virtual_environment_vm" "vm_k3s_server_04" {
 }
 resource "proxmox_virtual_environment_vm" "vm_k3s_server_05" {
     name = "VM-DOCKER-SERVER-05"
-    node_name = "HYPERVISOR-01"
+    node_name = "HYPERVISOR-02"
     vm_id = 205
     tags = ["docker", "glusterfs", "portainer", "ubuntu-2204"]
     on_boot = true
