dfc909006908eba92c1b455f6bbc741d3c660c87
Adam Beckett <becketta@live.com>
2024-03-18 21:08:10 +0000

    updated

--- proxmox/app-servers/resources.tf
+++ proxmox/app-servers/resources.tf
@@ -19,7 +19,7 @@ resource "proxmox_virtual_environment_vm" "vm_app_server-01" {
         sockets = 2
     }
     disk {
-        datastore_id = "storage-01-iscsi"
+        datastore_id = "storage-01-zfs-iscsi"
         interface = "scsi0"
         size = 100
     }
@@ -29,10 +29,12 @@ resource "proxmox_virtual_environment_vm" "vm_app_server-01" {
     network_device {
         bridge = "vmbr0"
         model = "virtio"
+        mac_address = "bc:24:11:3e:8a:54"
     }
     network_device {
         bridge = "vmbr1"
         model = "virtio"
+        mac_address = "bc:24:11:26:ef:fb"
     }
     initialization {
         datastore_id = "local-lvm"
@@ -42,6 +44,8 @@ resource "proxmox_virtual_environment_vm" "vm_app_server-01" {
                 address = "192.168.10.171/24"
                 gateway = "192.168.10.1"
             }
+        }
+        ip_config {
             ipv4 {
                 address = "192.168.20.171/24"
                 gateway = "192.168.20.1"
@@ -70,7 +74,7 @@ resource "proxmox_virtual_environment_vm" "vm_app_server-02" {
         sockets = 2
     }
     disk {
-        datastore_id = "storage-01-iscsi"
+        datastore_id = "storage-01-zfs-iscsi"
         interface = "scsi0"
         size = 100
     }
@@ -80,10 +84,12 @@ resource "proxmox_virtual_environment_vm" "vm_app_server-02" {
     network_device {
         bridge = "vmbr0"
         model = "virtio"
+        mac_address = "bc:24:11:01:9c:6e"
     }
     network_device {
         bridge = "vmbr1"
         model = "virtio"
+        mac_address = "bc:24:11:14:c5:06"
     }
     initialization {
         datastore_id = "local-lvm"
@@ -93,6 +99,8 @@ resource "proxmox_virtual_environment_vm" "vm_app_server-02" {
                 address = "192.168.10.172/24"
                 gateway = "192.168.10.1"
             }
+        }
+        ip_config {
             ipv4 {
                 address = "192.168.20.172/24"
                 gateway = "192.168.20.1"
