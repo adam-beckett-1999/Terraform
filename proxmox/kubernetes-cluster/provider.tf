2aae02726220f7997c443078a2c8108b65814dfb
Adam Beckett <becketta@live.com>
2024-11-21 13:21:07 +0000

    Update provider.tf to include latest version of bpg/proxmox provider.

--- proxmox/kubernetes-cluster/provider.tf
+++ proxmox/kubernetes-cluster/provider.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 0.13.0"
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
-      version = "0.46.2"
+      version = "0.67.1"
     }
   }
 }
